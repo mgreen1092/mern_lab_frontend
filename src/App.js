28b6cd2530e256779845fcbfffc08c4349199a1e
mgreen1092 <morgan.green0209@gmail.com>
2022-12-05 21:03:57 -0500

    edit and create fully working, delete has a bug

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import Create from './components/Create';
 import { Route, Routes } from 'react-router-dom'
 import Homepage from './components/Homepage';
+import Edit from './components/Edit'
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState({
+    title: '',
+    author: '',
+    description: ''
+  })
+  const [allBooks, setAllBooks]=useState()
   return (<div>
     <Routes>
-      <Route path='/' element={<Homepage />}></Route>
-      <Route path='/create' element={<Create />}></Route>
+      <Route path='/' element={<Homepage input={input} setInput={setInput} allBooks={allBooks} setAllBooks={setAllBooks}/>}></Route>
+      <Route path='/create' element={<Create input={input} setInput={setInput}/>}></Route>
+      <Route path='/edit/:id' element={<Edit input={input} setInput={setInput}/>}></Route>
     </Routes>
   </div>)
 }
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,15 +1,28 @@
 import React from "react";
+import axios from "axios";
 
-const Create = () => {
+const Create = (props) => {
+    const handleChange = async (event) => {
+        props.setInput ({
+            ...props.input,
+            [event.target.name]: event.target.value
+        })
+    }
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const response = await axios.post('http://localhost:4000/api/library/create/', props.input)
+        console.log(response)
+    }
     return(
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 Title:
-            <input type="text" name="title"></input>
+            <input onChange={handleChange} type="text" name="title"></input>
                 Author:
-            <input type="text" name="author"></input>
+            <input onChange={handleChange} type="text" name="author"></input>
                 Description:
-            <input type="text" name="description"></input>
+            <input onChange={handleChange} type="text" name="description"></input>
+            <button>Submit</button>
             </form>
         </div>
     )
